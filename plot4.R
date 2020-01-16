2160052cb85f3619b093636764e62a6ff293b598
sitisalwani <salwani1039@gmail.com>
2020-01-17 06:08:59 +0800

    Combine graph as plot4.R

--- plot4.R
+++ plot4.R
@@ -1,20 +1,26 @@
-## Create Plot 4
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(t, {
-  plot(Global_active_power~dateTime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
-  plot(Voltage~dateTime, type="l", 
-       ylab="Voltage (volt)", xlab="")
-  plot(Sub_metering_1~dateTime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~dateTime,col='Red')
-  lines(Sub_metering_3~dateTime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~dateTime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
-})
+
+# Create Multiple Plot
+
+par(mfrow = c(2, 2)) 
+
+# First plot
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+# Second plot
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+# Third plot
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+#legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", col=c("black", "red", "blue"), lwd=2.5, 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.1, lty = 1)
+
+
+# Fourth plot
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power", cex=0.2)
 
 ## Saving to file
-#dev.copy(png, file="plot4.png", height=480, width=480)
-#dev.off()
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
